8979583b12e885b673150f5fae78ae7f68a273c8
TNIFL <tnifl@naver.com>
2026-05-14 15:37:42 +0900

    0514

--- routes/haksa.js
+++ routes/haksa.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var {getConnection} = require('./connect');
 var oracledb = require('oracledb');
+const { get } = require('./haksa');
 
 /* 교수 관리 페이지 */
 router.get('/pro', function(req, res, next) {
@@ -34,7 +35,7 @@ router.get('/cou', function(req, res, next) {
 
 /* 학생관리 */
 router.get('/stu', function(req, res, next) {
-  res.render("index", {title: '학생관리', pageName: 'haksa/stu.ejs'});
+  res.render("index", {title: '학생관리', pageName: 'haksa/students.ejs'});
 });
 
 
@@ -81,6 +82,22 @@ router.post('/pro/delete', async function(req, res) {
   }
 })
 
+// 학생목록 데이터
+router.get('/stu/list.json', async function(req, res) {
+  let con;
+  try {
+    con = await getConnection();
+    let sql = "select * from view_students order by scode desc";
+    let result = await con.execute(sql, {}, {outFormat:oracledb.OUT_FORMAT_OBJECT});
+    res.send(result.rows);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  } finally {
+    if (con) await con.close();
+  }
+})
+
 /* 교수등록 페이지. */
 router.get('/pro/insert', async function(req, res, next) {
   let code;
@@ -97,5 +114,64 @@ router.get('/pro/insert', async function(req, res, next) {
   }
   res.render('index', {title:'교수등록', pageName:'haksa/professor_insert.ejs', code});
 });
+
+//학생 등록 페이지
+router.get('/stu/insert', async function(req, res) {
+  let con;
+  let code;
+  try {
+    con = await getConnection();
+    let sql = "select max(scode) + 1 from students";
+    let result = await con.execute(sql);
+    code = result.rows[0][0];
+  } catch (err) {
+
+  } finally {
+    if (con) await con.close();
+  }
+   res.render('index', {title: '학생입력', pageName:'haksa/students_insert.ejs', code});
+})
+
+//학생등록
+router.post('/stu/insert', async function(req, res) {
+  const scode = req.body.scode;
+  const sname = req.body.sname;
+  const dept = req.body.dept;
+  const birthday = req.body.birthday;
+  const year = req.body.year;
+  const advisor = req.body.pcode;
+  console.log(scode, sname, dept, birthday, year, advisor);
+  let con;
+  try {
+    con = await getConnection();
+    let sql = "insert into students(scode, sname, dept, birthday, year, advisor) values(:scode, :sname, :dept, :birthday, :year, :advisor)";
+    await con.execute(sql, {scode, sname, dept, birthday, year, advisor}, {autoCommit:true});
+    res.sendStatus(200);
+  } catch(err) {
+    console.error(err);
+    res.sendStatus(500);
+  } finally {
+    if (con) await con.close();
+  }
+})
+
+// 학생삭제
+router.post('/stu/delete', async function(req, res){
+    let con;
+    const scode=req.body.scode;
+    try{
+        con = await getConnection();
+        const sql=`delete from students where scode=${scode}`;
+        console.log(sql);
+        await con.execute(sql, {}, {autoCommit:true});
+        res.sendStatus(200);
+    }catch(err){
+        res.sendStatus(500);
+        console.log(err);
+    }finally{
+        if(con) await con.close();
+    }
+});
+
 
 module.exports = router;
